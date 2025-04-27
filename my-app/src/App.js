10ce3582ab420d103176c075354771a9b604c022
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-04-26 21:31:52 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,100 @@
+import CinemaCard from './CinemaCard.jsx';
+import React, { useState } from "react";
+import DatePickerComponent from './DatePickerComponent.jsx';
+import Componente from './componente.jsx';
+
+const sede1 = {
+    sede: 'CineAgile Trujillo',
+    funciones: [
+        {
+            dimension: "2D",
+            categoria: "Premium",
+            horaInicio: "4:00 pm",
+            sala: 1,
+            precio: 1000,
+        },
+        {
+            dimension: "2D",
+            categoria: "Premium",
+            horaInicio: "4:30 pm",
+            sala: 1,
+            precio: 1000,
+        },
+        {
+            dimension: "2D",
+            categoria: "Premium",
+            horaInicio: "5:30 pm",
+            sala: 1,
+            precio: 1000,
+        }
+    ]
+}
+const sede2 = {
+    sede: 'CineAgile Lima',
+    funciones: [
+        {
+            dimension: "2D",
+            categoria: "Premium",
+            horaInicio: "5:00 pm",
+            sala: 1,
+            precio: 1000,
+        },
+        {
+            dimension: "3D",
+            categoria: "Regular",
+            horaInicio: "5:30 pm",
+            sala: 1,
+            precio: 1000,
+        },
+        {
+            dimension: "3D",
+            categoria: "Regular",
+            horaInicio: "6:30 pm",
+            sala: 1,
+            precio: 1000,
+        }
+    ]
+}
+const sede3 = {
+    sede: 'CineAgile Cajamarca',
+    funciones: [
+        {
+            dimension: "3D",
+            categoria: "Premium",
+            horaInicio: "6:00 pm",
+            sala: 1,
+            precio: 1000,
+        },
+        {
+            dimension: "2D",
+            categoria: "Regular",
+            horaInicio: "7:30 pm",
+            sala: 1,
+            precio: 1000,
+        },
+        {
+            dimension: "2D",
+            categoria: "Premium",
+            horaInicio: "8:30 pm",
+            sala: 1,
+            precio: 1000,
+        }
+    ]
+}
+function App() {
+    return (
+        <div className="App p-4">
+            <div className="mx-3 d-flex justify-content-between">
+                <div>
+                    <h4>Pelicula</h4>
+                    <h4>Star Wars capitulo 3</h4>
+                </div>
+                <DatePickerComponent></DatePickerComponent>
+            </div>
+            <CinemaCard sede={sede1.sede} funciones={sede1.funciones}></CinemaCard>
+            <CinemaCard sede={sede2.sede} funciones={sede2.funciones}></CinemaCard>
+            <CinemaCard sede={sede3.sede} funciones={sede3.funciones}></CinemaCard>
+        </div>
+    );
+}
+export default App;
---
+++ my-app/src/CinemaCard.jsx
@@ -0,0 +1,88 @@
+import './CinemaCard.css';
+import React from 'react';
+import Screening from './Screening.jsx';
+
+const CinemaCard = ({ sede, funciones }) => {
+    return (
+        <div className='mx-3'>
+            <div className="card">
+                <button className="card-header" type="button" data-bs-toggle="collapse" data-bs-target={"#" + sede.replace(/\s+/g, '')}
+                    aria-expanded="false" aria-controls={sede.replace(/\s+/g, '')}>
+                    <h5 className='text-start'>
+                        {sede}
+                    </h5>
+                </button>
+                <div className="card-body collapse" id={sede.replace(/\s+/g, '')}>
+                    {funciones.some((funcion) => funcion.dimension === "2D" && funcion.categoria === "Regular")
+                        ?
+                        <>
+                            <h5 className="card-title">2D Regular</h5>
+                            <div className="d-flex row mb-2">
+                                {Object.keys(funciones).map((key) => {
+                                    if (funciones[key].dimension === "2D" && funciones[key].categoria === "Regular") {
+                                        return <Screening funcion={funciones[key]}></Screening>
+
+                                    }
+                                })}
+                            </div>
+                        </>
+                        :
+                        <></>
+                    }
+                    {funciones.some((funcion) => funcion.dimension === "2D" && funcion.categoria === "Premium")
+                        ?
+                        <>
+                            <h5 className="card-title">2D Premium</h5>
+                            <div className="d-flex row mb-2">
+                                {Object.keys(funciones).map((key) => {
+                                    if (funciones[key].dimension === "2D" && funciones[key].categoria === "Premium") {
+                                        return <Screening funcion={funciones[key]}></Screening>
+
+                                    }
+                                })}
+                            </div>
+                        </>
+                        :
+                        <></>
+                    }
+                    {funciones.some((funcion) => funcion.dimension === "3D" && funcion.categoria === "Regular")
+                        ?
+                        <>
+                            <h5 className="card-title">3D Regular</h5>
+                            <div className="row mb-2">
+                                {Object.keys(funciones).map((key) => {
+                                    if (funciones[key].dimension === "3D" && funciones[key].categoria === "Regular") {
+                                        return <Screening funcion={funciones[key]}></Screening>
+
+
+
+                                    }
+                                })}
+                            </div>
+                        </>
+                        :
+                        <></>
+                    }
+                    {funciones.some((funcion) => funcion.dimension === "3D" && funcion.categoria === "Premium")
+                        ?
+                        <>
+                            <h5 className="card-title">3D Regular</h5>
+                            <div className="row mb-2">
+                                {Object.keys(funciones).map((key) => {
+                                    if (funciones[key].dimension === "3D" && funciones[key].categoria === "Premium") {
+                                        return <Screening funcion={funciones[key]}></Screening>
+
+                                    }
+                                })}
+                            </div>
+                        </>
+                        :
+                        <></>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CinemaCard;
