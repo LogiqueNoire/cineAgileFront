4a6f3c20ad43672ee69551839e73b7195928cc88
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-04-27 19:10:10 -0500

    borrar ejemplo del app.js

--- src/App.js
+++ src/App.js
@@ -6,58 +6,6 @@ import FilmTab from './FilmTab.jsx';
 import ComJose1 from './ComJose1.jsx';
 import ComJose2 from './ComJose2.jsx';
 
-/*    @Column
-    @Temporal(TemporalType.DATE)      
-    LocalDate   fechaInicioPreventa;
-    @Column
-    @Temporal(TemporalType.DATE)
-    LocalDate   fechaInicioEstreno;
-    @Column
-    String estado;*/
-//"En cartelera", "Próximamente", "Preventa", "No visible"
-const peliculas = {
-
-[
-    { id: 1, nombre: "Pelicula 1", sinopsis: "Descripcion de la pelicula 1", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen1.png', estado: "En cartelera" },
-    { id: 2, nombre: "Pelicula 2", sinopsis: "Descripcion de la pelicula 2", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen2.png', estado: "En cartelera" },
-    { id: 3, nombre: "Pelicula 3", sinopsis: "Descripcion de la pelicula 3", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen3.png', estado: "En cartelera" },
-    { id: 4, nombre: "Pelicula 4", sinopsis: "Descripcion de la pelicula 4", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen4.png', estado: "Proximamente" },
-    { id: 5, nombre: "Pelicula 5", sinopsis: "Descripcion de la pelicula 5", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen5.png', estado: "Proximamente" },
-    { id: 6, nombre: "Pelicula 6", sinopsis: "Descripcion de la pelicula 6", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen6.png', estado: "En cartelera" },
-    { id: 7, nombre: "Pelicula 7", sinopsis: "Descripcion de la pelicula 7", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen7.png', estado: "En cartelera" },
-    { id: 8, nombre: "Pelicula 8", sinopsis: "Descripcion de la pelicula 8", genero: '', director: '', clasificacion:'', actores:'', imagen:'/img/Imagen1.png', estado: "En cartelera" },
-
-]
-}
-
-sedes:[
-    {    sede: 'CineAgile Trujillo',
-    funciones: [
-        {            dimension: "2D",            categoria: "Premium",            horaInicio: "4:00 pm",            sala: 1,            precio: 1000,        },
-        {            dimension: "2D",            categoria: "Premium",            horaInicio: "4:30 pm",            sala: 1,            precio: 1000,        },
-        {            dimension: "3D",            categoria: "Regular",            horaInicio: "5:30 pm",            sala: 1,            precio: 1000,        }
-    ]
-},
-{
-
-    sede: 'CineAgile Lima',
-    funciones: [
-        {            dimension: "2D",            categoria: "Premium",            horaInicio: "5:00 pm",            sala: 1,            precio: 1000,        },
-        {            dimension: "3D",            categoria: "Regular",            horaInicio: "5:30 pm",            sala: 1,            precio: 1000,        },
-        {            dimension: "3D",            categoria: "Regular",            horaInicio: "6:30 pm",            sala: 1,            precio: 1000,        }
-    ]
-},
-{
-    sede: 'CineAgile Cajamarca',
-    funciones: [
-        {            dimension: "3D",            categoria: "Premium",            horaInicio: "6:00 pm",            sala: 1,            precio: 1000,        },
-        {            dimension: "3D",            categoria: "Regular",            horaInicio: "7:30 pm",            sala: 1,            precio: 1000,        },
-        {            dimension: "3D",            categoria: "Premium",            horaInicio: "8:30 pm",            sala: 1,            precio: 1000,        }
-    ]
-}]
-
-
-
 function App() {
     return (
         <div className="App p-4">
