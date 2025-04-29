1a62d54aebb439fcd4c0a9f5e22a03bcd132199c
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-04-29 17:59:22 -0500

    mover ComJose1

--- src/precios/ComJose1.jsx
+++ src/precios/ComJose1.jsx
@@ -1,14 +1,43 @@
 import React from "react";
+import { useState } from "react";
 const ComJose1 = ({ nombre, texto, precio }) => {
-    return (
+    const [cantidad, setCantidad] = useState(0);
 
-        <div className="nombre">
-            <strong>{nombre}</strong>
-            <div className="texto">
-                <h3>{texto}</h3>
-                <div className="precio">
-                    <h3>{precio} </h3>
+    const agregar = () => {
+        setCantidad(cantidad + 1);
+    };
+
+    const restar = () => {
+        if (cantidad > 0) {
+            setCantidad(cantidad - 1);
+        }
+    };
+    return (
+        <div className="d-flex">
+            <div className="nombre w-50">
+                <h2 className="text-center">{nombre}</h2>
+                <div className="texto w-100">
+                    <p className="text-center">{texto}</p>
+                    <div className="precio w-100 text-center">
+                        <p>{precio} </p>
+                    </div>
                 </div>
+            </div>
+
+            <div className="d-flex align-middle w-50">
+            <button className="botonQuitar d-flex m-2 border-0 bg-transparent" onClick={restar}  >
+                <div className="botonrestar d-flex">
+                    <img src="https://cdn-icons-png.flaticon.com/512/66/66889.png" alt="boton menos" height="30px"></img>
+                </div>
+            </button>
+
+            <h3 className="contador align-middle m-2 ">{cantidad}</h3>
+
+            <button className="botonAgregar d-flex m-2 border-0 bg-transparent" onClick={agregar}>
+                <div className="botonsuma d-flex">
+                    <img src="https://cdn-icons-png.flaticon.com/512/117/117885.png" alt="boton mas" height="30px" ></img>
+                </div>
+            </button>
             </div>
         </div>
     );
