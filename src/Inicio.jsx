912f8daf671a0391b47d2b22fd77a854ec866a3c
spiderdog22 <spiderdog22@gmail.com>
2025-04-29 01:45:47 -0500

    Despejar componente Inicio

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,17 +1,11 @@
 
 import './Inicio.css'
 import FilmPanel from './componentesCartelera/FilmPanel'
-import Header from './componentesGenerales/Header'
 
 const Inicio = () => {
 
     return (<>
-        <Header />
         <FilmPanel />
-
-        <footer>
-            Pies
-        </footer>
     </>)
 };
 
