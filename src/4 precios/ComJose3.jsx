da270901216e9f13e41fb806aa6f24b71dc78185
calingod <joguisaz454@outlook.com>
2025-04-30 18:01:20 -0500

    color

--- src/4 precios/ComJose3.jsx
+++ src/4 precios/ComJose3.jsx
@@ -8,7 +8,7 @@ import React from "react";
             <div className=" d-flex justify-content-center"> 
             <img  className=""src="https://i.pinimg.com/736x/8d/60/e9/8d60e928c17f67d67683bd5262000a5c.jpg" alt="imagen" height="200px" ></img>
             </div>
-            <h1 className="text-center  m-4">{nombrePeli}</h1> 
+            <h1 className="text-center  m-4"style={{ color: 'blue' }}>{nombrePeli}</h1> 
             <h2 className="text-center  m-3">{catePeli}</h2>
             <h3 className="text-center  m-3">{sedePeli}</h3>
             <h3 className="text-center  m-3">{fechaPeli}</h3>
