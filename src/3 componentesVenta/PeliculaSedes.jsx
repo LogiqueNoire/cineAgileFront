f8c0abaedbbe908bea8716129cb52b61a61ddd0e
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-05-01 18:19:02 -0500

    cambios input fecha

--- src/3 componentesVenta/PeliculaSedes.jsx
+++ src/3 componentesVenta/PeliculaSedes.jsx
@@ -1,10 +1,17 @@
 import { useLocation } from "react-router";
 import MostrarSedesHorarios from "./MostrarSedesHorarios";
+import { format } from 'date-fns'
 
 const PeliculaSedes = () => {
     const location = useLocation();
     const { consultaIdPelicula, nombrePelicula, imagenPeli } = location.state || {};
     console.log(consultaIdPelicula)
+    
+    // Formatear la fecha en formato 'yyyy-MM-ddTHH:mm'
+    const now = new Date().toISOString().slice(0, 19);
+    let fechaFormateada = format(now, `yyyy-MM-dd.HH:mm`).replace('.', 'T')
+    const [fecha, setfecha] = useState(fechaFormateada);
+
 
     return (<>
         <div className="d-flex justify-content-center align-items-center p-4 bg-light mb-4">
@@ -12,13 +19,13 @@ const PeliculaSedes = () => {
                 <h2 className='mb-4'>Funciones para película {nombrePelicula}</h2>
                 <div>
                     <h5 className="mx-3">Fecha:</h5>
-                    <input type="date" className="mx-3" />
+                    <input type="date" className="mx-3" min={fechaFormateada} value={fecha} onChange={(e)=>setfecha(e.target.value)}/>
                 </div>
             </div>
             <img src={imagenPeli} alt="imagen Peli" />
         </div>
 
-        <MostrarSedesHorarios estado={ location.state } />
+        <MostrarSedesHorarios estado={ location.state} fechaFormateada={fechaFormateada} />
     </>);
 };
 
