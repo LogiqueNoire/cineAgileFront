2cb72f0bdf0552f6a07f6fbe10d30d00a618baf1
spiderdog22 <spiderdog22@gmail.com>
2025-05-01 14:29:41 -0500

    Estructura básica del flujo de compra

--- src/3 componentesVenta/FlujoVenta.jsx
+++ src/3 componentesVenta/FlujoVenta.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router";
 
 import ButacaSelect from './ButacaSelect'
 import { VentaContextProvider } from './VentaContextProvider'
+import ComJose3 from '../4 precios/ComJose3'
 
 const FlujoVenta = () => {
     const navigate = useNavigate()
@@ -14,7 +15,14 @@ const FlujoVenta = () => {
 
     return (<>
         <VentaContextProvider>
-            { pasosCompra[0] }        
+
+            <div className="compra-info">
+                <ComJose3 />
+            </div>
+
+            <div className="compra-step">
+                { pasosCompra[0] }        
+            </div>
 
             <button onClick={ () => { navigate(-1) } } >Volver</button>
             <button>Siguiente</button>
