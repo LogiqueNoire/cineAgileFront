f494c6ed750fd1eeff6feb91029ba80e5f47ee88
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-05-01 20:16:12 -0500

    .

--- src/3 componentesVenta/PeliculaSedes.jsx
+++ src/3 componentesVenta/PeliculaSedes.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from "react-router";
 import { useRef, useState } from "react";
 import MostrarSedesHorarios from "./MostrarSedesHorarios";
-import { useState } from "react";
 import { format } from 'date-fns'
 
 const formatearTiempo = (fecha) => {
