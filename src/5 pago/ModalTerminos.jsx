abafab39130d4aa5163979d82450f8c541050b50
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-05-21 13:48:20 -0500

    Info modal actualizada

--- src/5 pago/ModalTerminos.jsx
+++ src/5 pago/ModalTerminos.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './pago.css';
 
 export const ModalTerminos = ({ onClose }) => {
@@ -7,31 +6,22 @@ export const ModalTerminos = ({ onClose }) => {
       <div className="modal">
         <h3 className="modal-title">Términos y Condiciones</h3>
         <div>
-          <strong>Aceptación de Términos:</strong> Al comprar en línea en Cineplanet, el usuario acepta los términos y condiciones.
+          <strong>Aceptación de Términos:</strong> Al comprar en línea en CineAgile, el usuario acepta los términos y condiciones.
         </div>
         <div>
-          <strong>Compra y Devoluciones:</strong> Las compras no son reembolsables ni modificables. Los productos deben recogerse el mismo día y se aplican cargos adicionales.
+          <strong>Compra y Devoluciones:</strong> Las compras no son reembolsables ni modificables.
         </div>
         <div>
-          <strong>Métodos de Pago:</strong> El pago se realiza con tarjeta de crédito o débito, o mediante billeteras digitales. Algunas tarjetas pueden ser rechazadas, y no se aceptan tarjetas extranjeras.
+          <strong>Métodos de Pago:</strong> El pago se realiza con tarjeta de crédito o débito, o mediante billeteras digitales. Algunas tarjetas pueden ser rechazadas y no se aceptan tarjetas extranjeras.
         </div>
         <div>
-          <strong>Registro de Socio:</strong> No obligatorio, pero permite beneficios. El usuario es responsable de la seguridad de su cuenta.
+          <strong>Ingreso a la Sala:</strong> Se requiere la entrada para ingresar a la sala. El personal validará los datos de la entrada mediante el escaneo del QR y, de ser necesario, validará los datos de la persona. Ante cualquier falta a la verdad determinada por el personal durante el ingreso o verificación manual, la empresa se reserva el derecho a impedir el ingreso de aquellas personas sin derecho a reembolso bajo ningún criterio.
         </div>
         <div>
-          <strong>Ingreso a la Sala:</strong> Se requiere comprobante de compra para ingresar. Está prohibido el ingreso de alimentos, bebidas externas, armas o cámaras.
+          <strong>Elementos Prohibidos:</strong> Está prohibido el ingreso de bebidas alcohólicas y armas.
         </div>
         <div>
-          <strong>Normas de Comportamiento:</strong> Se prohíben comportamientos peligrosos o de acoso. El cine puede negar el acceso a quienes no respeten las normas.
-        </div>
-        <div>
-          <strong>Política de Reembolsos:</strong> Solo se aceptan devoluciones por doble cobro o falta de confirmación. No se aceptan devoluciones de entradas anticipadas.
-        </div>
-        <div>
-          <strong>Canjes Online:</strong> No se permiten cambios ni devoluciones de boletos corporativos por error. Existen boletos corporativos totales y parciales.
-        </div>
-        <div>
-          <strong>Discapacidad:</strong> Personas con discapacidad acreditada tienen un descuento del 20% en entradas, con presentación de carnet CONADIS y DNI.
+          <strong>Normas de Comportamiento:</strong> Se prohíben comportamientos peligrosos o de acoso. El cine puede negar el acceso a quienes no respeten las normas. No se permite grabar total o parcialmente el contenido de la función.
         </div>
         <button className="close-button" onClick={onClose}>
           Cerrar
