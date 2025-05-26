2e09e1bf9176cb5071bf7069d222c0d4be80c691
LogiqueNoire <139007271+LogiqueNoire@users.noreply.github.com>
2025-05-26 13:12:52 -0500

    quitar billeteras de ModalTerminos.jsx

--- src/5 6 pago/ModalTerminos.jsx
+++ src/5 6 pago/ModalTerminos.jsx
@@ -12,7 +12,7 @@ export const ModalTerminos = ({ onClose }) => {
           <strong>Compra y Devoluciones:</strong> Las compras no son reembolsables ni modificables.
         </div>
         <div>
-          <strong>Métodos de Pago:</strong> El pago se realiza con tarjeta de crédito o débito, o mediante billeteras digitales. Algunas tarjetas pueden ser rechazadas y no se aceptan tarjetas extranjeras.
+          <strong>Métodos de Pago:</strong> El pago se realiza con tarjeta de crédito o débito. Algunas tarjetas pueden ser rechazadas y no se aceptan tarjetas extranjeras.
         </div>
         <div>
           <strong>Ingreso a la Sala:</strong> Se requiere la entrada para ingresar a la sala. El personal validará los datos de la entrada mediante el escaneo del QR y, de ser necesario, validará los datos de la persona. Ante cualquier falta a la verdad determinada por el personal durante el ingreso o verificación manual, la empresa se reserva el derecho a impedir el ingreso de aquellas personas sin derecho a reembolso bajo ningún criterio.
