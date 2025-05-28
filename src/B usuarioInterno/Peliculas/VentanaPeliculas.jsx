1b3ccd6ec5193f56c44a36a028cdb1f710b7cf7f
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-05-28 12:02:48 -0500

    Loading en consultar

--- src/B usuarioInterno/Peliculas/VentanaPeliculas.jsx
+++ src/B usuarioInterno/Peliculas/VentanaPeliculas.jsx
@@ -3,9 +3,11 @@ import AddFilm from './AddFilm';
 import './VentanaPeliculas.css'
 import axios from 'axios';
 import { url } from "../../configuracion/backend"
+import Loading from '../../0 componentesGenerales/Loading';
 
 const VentanaPeliculas = () => {
     const [lista, setLista] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const consultar = async () => {
         try {
@@ -21,6 +23,7 @@ const VentanaPeliculas = () => {
 
 
     useEffect(() => {
+        setLoading(false)
         console.log(lista)
     }, [lista])
 
@@ -29,48 +32,51 @@ const VentanaPeliculas = () => {
             <div className='d-block'>
                 <AddFilm onSucess={consultar}></AddFilm>
 
-                <table className='table mytable table-striped border table-hover mt-4'>
-                    <thead className='thead'>
-                        <tr>
-                            <td className='td'>Nombre</td>
-                            <td className='td'>Duración (min)</td>
-                            <td className='td' style={{width: "400px"}}>Sinopsis</td>
-                            <td className='td'>Género</td>
-                            <td className='td'>Director</td>
-                            <td className='td'>Clasificación</td>
-                            <td className='td'>Estado</td>
-                            <td className='td'>Actores</td>
-                            <td className='td'>Inicio de preventa</td>
-                            <td className='td'>Inicio de estreno</td>
-                            <td className='td'>Imagen</td>
-                        </tr>
-                    </thead>
-                    <tbody className='tbody'>
-                        {lista.map((el, id) => (
-
-                            <tr className='tr' key={id}>
-                                <td className='td' data-label='Nombre'>{el.nombre}</td>
-                                <td className='td' data-label='Duración'>{el.duracion}</td>
-                                <td className='td' data-label='Sinopsis'><div className='sinopsis'>{el.sinopsis}</div></td>
-                                <td className='td' data-label='Género'>{el.genero === " " ? "-" : el.genero}</td>
-                                <td className='td' data-label='Director'>{el.director === " " ? "-" : el.director}</td>
-                                <td className='td' data-label='Clasificación'>{el.clasificacion === " " ? "-" : el.clasificacion}</td>
-                                <td className='td' data-label='Estado'>{el.estado}</td>
-                                <td className='td' data-label='Actores'>{el.actores === " " ? "-" : el.actores}</td>
-                                <td className='td' data-label='Inicio de preventa'>{el.fechaInicioPreventa}</td>
-                                <td className='td' data-label='Inicio de estreno'>{el.fechaInicioEstreno}</td>
-                                <td className='td' data-label='Imagen'>
-                                    <a href={el.imageUrl} target="_blank" rel="noopener noreferrer">
-                                        Enlace
-                                    </a>
-
-                                </td>
+                {loading == true
+                ? <Loading></Loading> : 
+                    <table className='table mytable table-striped border table-hover mt-4'>
+                        <thead className='thead'>
+                            <tr>
+                                <td className='td'>Nombre</td>
+                                <td className='td'>Duración (min)</td>
+                                <td className='td' style={{ width: "400px" }}>Sinopsis</td>
+                                <td className='td'>Género</td>
+                                <td className='td'>Director</td>
+                                <td className='td'>Clasificación</td>
+                                <td className='td'>Estado</td>
+                                <td className='td'>Actores</td>
+                                <td className='td'>Inicio de preventa</td>
+                                <td className='td'>Inicio de estreno</td>
+                                <td className='td'>Imagen</td>
                             </tr>
+                        </thead>
+                        <tbody className='tbody'>
+                            {lista.map((el, id) => (
 
-                        ))}
-                    </tbody>
+                                <tr className='tr' key={id}>
+                                    <td className='td' data-label='Nombre'>{el.nombre}</td>
+                                    <td className='td' data-label='Duración'>{el.duracion}</td>
+                                    <td className='td' data-label='Sinopsis'><div className='sinopsis'>{el.sinopsis}</div></td>
+                                    <td className='td' data-label='Género'>{el.genero === " " ? "-" : el.genero}</td>
+                                    <td className='td' data-label='Director'>{el.director === " " ? "-" : el.director}</td>
+                                    <td className='td' data-label='Clasificación'>{el.clasificacion === " " ? "-" : el.clasificacion}</td>
+                                    <td className='td' data-label='Estado'>{el.estado}</td>
+                                    <td className='td' data-label='Actores'>{el.actores === " " ? "-" : el.actores}</td>
+                                    <td className='td' data-label='Inicio de preventa'>{el.fechaInicioPreventa}</td>
+                                    <td className='td' data-label='Inicio de estreno'>{el.fechaInicioEstreno}</td>
+                                    <td className='td' data-label='Imagen'>
+                                        <a href={el.imageUrl} target="_blank" rel="noopener noreferrer">
+                                            Enlace
+                                        </a>
 
-                </table>
+                                    </td>
+                                </tr>
+
+                            ))}
+                        </tbody>
+
+                    </table>
+                }
             </div>
         </div>
     )
--- src/B usuarioInterno/SedesYSalas/VentanaSedesYSalas.jsx
+++ src/B usuarioInterno/SedesYSalas/VentanaSedesYSalas.jsx
@@ -22,7 +22,7 @@ const VentanaSedesYSalas = () => {
         //navigate(`/intranet/sedesysalas/${idSede}`)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("hola")
         consultar()
     }, [])
@@ -35,29 +35,31 @@ const VentanaSedesYSalas = () => {
         <div>
             <div className='d-flex flex-column align-items-center container'>
                 <AddSede onSucess={consultar}></AddSede>
-
-                <table className='table table-striped border table-hover m-4'>
-                    <thead className=''>
-                        <tr className=''>
-                            <td className=''>Nombre de sede</td>
-                            <td className=''></td>
-                        </tr>
-                    </thead>
-                    <tbody className=''>
-                        {lista.map((el, id) => (
-
-                            <tr key={el.id}>
-                                <td>{el.nombre}</td>
-                                <td className='text-center'>
-                                    <button className='btn btn-primary' onClick={() => moverse(el.id)}>Ver</button>
-                                </td>
+                {loading == true
+                    ? <Loading></Loading> :
+                    <table className='table table-striped border table-hover m-4'>
+                        <thead className=''>
+                            <tr className=''>
+                                <td className=''>Nombre de sede</td>
+                                <td className=''></td>
                             </tr>
+                        </thead>
+                        <tbody className=''>
+                            {lista.map((el, id) => (
+
+                                <tr key={el.id}>
+                                    <td>{el.nombre}</td>
+                                    <td className='text-center'>
+                                        <button className='btn btn-primary' onClick={() => moverse(el.id)}>Ver</button>
+                                    </td>
+                                </tr>
 
 
-                        ))}
-                    </tbody>
+                            ))}
+                        </tbody>
 
-                </table>
+                    </table>
+                }
             </div>
         </div>
     )
