cd085bb9d5b7f52e3fc95fbd589d07e69801739c
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-06-05 20:49:43 -0500

    Cronograma 70%

--- src/B usuarioInterno/Funciones/Cronograma.jsx
+++ src/B usuarioInterno/Funciones/Cronograma.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import MostrarSedesHorarios from "../../3 componentesVenta/MostrarSedesHorarios";
 
 const Cronograma = ({ funciones, fechaConsultada }) => {
   if (!funciones.length) return null;
@@ -14,6 +15,21 @@ const Cronograma = ({ funciones, fechaConsultada }) => {
   }
   console.log("fechasSemana", fechasSemana);
 
+  const horas = [];
+
+  for (let h = 7; h <= 23; h++) {
+    const fecha = new Date();
+    fecha.setHours(h, 0, 0);
+    horas.push(new Date(fecha));
+  }
+
+  function formatearHora(fechaStr) {
+    const fecha = new Date(fechaStr);
+    const h = fecha.getHours().toString().padStart(2, '0');
+    const m = fecha.getMinutes().toString().padStart(2, '0');
+    return `${h}:${m}`;
+  }
+
   return (
     <section className=''>
       <h2 className='d-none d-lg-block mr-4'>Funciones</h2>
@@ -27,31 +43,48 @@ const Cronograma = ({ funciones, fechaConsultada }) => {
                 </div>
               </th>
               {fechasSemana.map((_, index) => (
-                <th key={index} scope="col" style={{ backgroundColor: 'rgb(184, 248, 255)' }}>
+                <th className='text-center' key={index} scope="col" style={{ backgroundColor: 'rgb(184, 248, 255)' }}>
                   {fechasSemana[index] ? `${diasDeLaSemana[index]} ${fechasSemana[index].toString().padStart(2, '0')}` : ''}
                 </th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {funciones.map((el, key) => (
-              <tr>
-                <td>{key}</td>
-                {fechasSemana.map((fs, index) => (
-                  console.log("funciones", (new Date(el.fechaHoraInicio)).getDay() + 1),
-                  (new Date(el.fechaHoraInicio)).getDay() + 1 === fs
-                    ?
-                    <td className='text-center' style={{ backgroundColor: 'rgb(184, 248, 255)' }}>
-                      <h5>{'Funcion ' + el.idFuncion}</h5>
-                      <h5>{el.categoria + el.dimension}</h5>
-                      <h5>{'Sala '+el.codigoSala}</h5>
-                      <h5></h5>
-                    </td>
-                    :
-                    <td></td>
-                ))}
-              </tr>
-            ))}
+            {
+              horas.map((hora, index) => (
+                <tr>
+                  <td>{hora.getHours()}</td>
+                  {
+
+
+                    fechasSemana.map((fs, index) => (
+                      <td>
+                        <div className='d-flex flex-column align-items-center gap-2'>
+
+                          {
+                          funciones.map((el, key) => (
+                            (new Date(el.fechaHoraInicio)).getDay() + 1 === fs && hora.getHours() === (new Date(el.fechaHoraInicio)).getHours()
+                              ?
+                              <td className='text-center' style={{ backgroundColor: 'rgb(184, 248, 255)' }}>
+                                <h6>{'Funcion ' + el.idFuncion}</h6>
+                                <h6>{formatearHora(el.fechaHoraInicio)}</h6>
+                                <h6>{el.categoria + ' ' + el.dimension}</h6>
+                                <h6>{'Sala ' + el.codigoSala}</h6>
+                                <h6></h6>
+                              </td>
+                              :
+                              <></>
+                          ))
+                          }
+                        </div>
+                      </td>
+                    ))
+
+                    
+                  }
+                </tr>
+              ))
+            }
           </tbody>
         </table>
       </div>
