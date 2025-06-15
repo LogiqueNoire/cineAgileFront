35fc30816c846e1e56bbde56d7033d0497b02c9c
spiderdog22 <spiderdog22@gmail.com>
2025-06-15 07:55:15 -0500

    Mejora exp. del formulario login

--- src/B usuarioInterno/LoginForm.jsx
+++ src/B usuarioInterno/LoginForm.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import Auth from "../servicios/Auth";
 import Cookies from "js-cookie";
 import { useNavigate, Navigate } from "react-router-dom";
-import Loading from "../0 componentesGenerales/Loading";
+import BotonCarga from "../0 componentesGenerales/BotonCarga"
 
 const LoginForm = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [status, setStatus] = useState("");
+    const [status, setStatus] = useState({ isError: false, details: null });
+
+    const [ submitting, setSubmitting ] = useState(false);
 
     useEffect(() => {
         if (Cookies.get("auth-token")) {
@@ -18,20 +20,27 @@ const LoginForm = () => {
 
     const tryLogin = (evt) => {
         evt.preventDefault();
-        setStatus("loading");
+
+        if (submitting) return;
+
+        setSubmitting(true);
+        setStatus({ isError: false, details: null });
+
         if (!Cookies.get("auth-token")) {
             Auth.login(username, password).then(res => {
                 if (res.token) {
                     Cookies.set("auth-token", res.token, { expires: 30 * 24 * 60 * 60, path: "/" });
-                    setStatus("ok");
                     navigate("/intranet");
                 }
-            }).catch(_ => {
-                setStatus("error");
+            }).catch(err => {
+                if (err.code == "ERR_NETWORK")
+                    setStatus({ isError: true, details: "Conexión de red." });
+                else
+                    setStatus({ isError: true, details: err.response.data.error });
+            }).finally(_ => {
+                setSubmitting(false);
             })
         }
-
- 
     }
 
     const onUsernameChange = (evt) => {
@@ -44,23 +53,30 @@ const LoginForm = () => {
 
     return (
         <div className="d-flex justify-content-center">
-            {status === "loading" ? <Loading></Loading> :
             <form className='d-flex flex-column align-items-center gap-3 m-4 border p-3 rounded' style={{ width: "max-content" }}
                 action="/intranet" method="post" onSubmit={tryLogin}>
+                { status.isError &&
+                    <div className="bg-danger bg-opacity-10 border border-1 border-danger w-100 p-2">
+                        <div className="text-center text-danger">{ status.details }</div>
+                    </div>
+                }
                 <h2>Cine Agile</h2>
-                <div className='d-flex flex-column align-items-center'>
-                    <label htmlFor="username">Username</label>
+                <div className='d-flex flex-column'>
+                    <label htmlFor="username">Usuario</label>
                     <input className='form-control' type="text" name="username" id="username" onChange={onUsernameChange} required />
                 </div>
 
-                <div className='d-flex flex-column align-items-center'>
-                    <label htmlFor="password">Password</label>
+                <div className='d-flex flex-column'>
+                    <label htmlFor="password">Contraseña</label>
                     <input className='form-control' type="password" name="password" id="password" onChange={onPasswordChange} required />
                 </div>
 
-                <button className='btn btn-primary' type="submit">Enviar</button>
 
-            </form>}
+                <BotonCarga submitting={submitting} type="submit" className='btn btn-primary'>
+                    Enviar
+                </BotonCarga>
+
+            </form>
         </div>
     )
 }
