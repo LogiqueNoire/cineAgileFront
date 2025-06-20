7d6f7c1023dc4369eb7a7d60731ad95440e208d0
spiderdog22 <spiderdog22@gmail.com>
2025-06-20 12:27:17 -0500

    Utilizar IDs de sedes para el acordion

--- src/2 componentesAcordionSedesHorarios/CinemaAcordion.jsx
+++ src/2 componentesAcordionSedesHorarios/CinemaAcordion.jsx
@@ -2,6 +2,7 @@ import './CinemaAcordion.css';
 import ScreeningButton from './ScreeningButton.jsx';
 
 const CinemaAcordion = ({ data, pelicula }) => {
+    console.log(data);
     return (
         <div className="mx-3">
             {data.funciones.map((sede) => (
@@ -10,14 +11,16 @@ const CinemaAcordion = ({ data, pelicula }) => {
                         className="card-header"
                         type="button"
                         data-bs-toggle="collapse"
-                        data-bs-target={`#${sede.nombreSede.replace(/\s+/g, '')}`}
+                        data-bs-target={ "#s" + sede.idSede } //`#${sede.nombreSede.replace(/\s+/g, '')}`}
                         aria-expanded="false"
-                        aria-controls={sede.nombreSede.replace(/\s+/g, '')}
+                        aria-controls={ `s${sede.idSede}` } //sede.nombreSede.replace(/\s+/g, '')}
                     >
                         <h5 className='text-start h5'>{sede.nombreSede}</h5>
                     </button>
 
-                    <div className="card-body collapse" id={sede.nombreSede.replace(/\s+/g, '')}>
+                    <div className="card-body collapse" 
+                        id={ `s${sede.idSede}` } // sede.nombreSede.replace(/\s+/g, '')  }>
+                    >
                         {["2D Regular", "2D Prime", "3D Regular", "3D Prime"].map((tipo) => {
                             const [dimension, categoria] = tipo.split(" ");
 
