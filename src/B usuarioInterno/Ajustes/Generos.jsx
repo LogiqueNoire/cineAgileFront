cdf6c98336a6737ed6f77abaab823df5926d24ed
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-06-24 00:54:45 -0500

    Varios géneros por película parte 2

--- src/B usuarioInterno/Ajustes/Generos.jsx
+++ src/B usuarioInterno/Ajustes/Generos.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loading from "../../0 componentesGenerales/Loading";
+import axios from "axios";
+import { url } from "../../configuracion/backend";
+import Cookies from "js-cookie";
+import iconoGuardar from "../../assets/guardar.svg"
+import Toast from "../../Toast";
+
 const Generos = () => {
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
     const [generos, setGeneros] = useState({});
+    const [generoNombre, setGeneroNombre] = useState(null)
+    const [toast, setToast] = useState({ visible: false, tipo: '', titulo: '', mensaje: '' })
 
     const consultarGeneros = async () => {
         try {
@@ -18,8 +26,87 @@ const Generos = () => {
         }
     }
 
+    const agregarGenero = async (e, nombre) => {
+        e.preventDefault();
+        let response;
+        try {
+            response = await axios.post(`${url}/intranet/generos/agregar`, nombre, {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("auth-token")}`,
+                    'Content-Type': 'text/plain'
+                }
+            });
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            consultarGeneros()
+            if (response) {
+                
+                setToast({
+                    visible: true,
+                    tipo: 'toast-info',
+                    titulo: 'Género agregado',
+                    mensaje: ''
+                })
+                setTimeout(() => setToast({ visible: false }), 3000);
+            } else {
+                setToast({
+                    visible: true,
+                    tipo: 'toast-danger',
+                    titulo: 'Error con el género',
+                    mensaje: 'Nombre repetido'
+                })
+                setTimeout(() => setToast({ visible: false }), 3000);
+            }
+
+        }
+    }
+
+    const editarGenero = async (e, el) => {
+        e.preventDefault();
+        let response;
+        try {
+            response = await axios.patch(`${url}/intranet/generos/editar`, {id: el.id, nombre: el.nombre}, {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("auth-token")}`
+                }
+            });
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            consultarGeneros()
+            if (response) {
+                
+                setToast({
+                    visible: true,
+                    tipo: 'toast-info',
+                    titulo: 'Género modificado',
+                    mensaje: ''
+                })
+                setTimeout(() => setToast({ visible: false }), 3000);
+            } else {
+                setToast({
+                    visible: true,
+                    tipo: 'toast-danger',
+                    titulo: 'Error con el género',
+                    mensaje: 'Nombre repetido'
+                })
+                setTimeout(() => setToast({ visible: false }), 3000);
+            }
+
+        }
+    }
+
+    useEffect(() => {
+        consultarGeneros()
+    }, [])
+
+
     return (
-        <div>
+        <div className="d-flex flex-column align-items-center container-fluid col-10 bg-white border border-3 p-5 rounded rounded-3 shadow mt-4">
+            <h2>Géneros registrados</h2>
             {loading === true
                 ? <div className='d-flex flex-column align-items-center container'><Loading></Loading></div> :
                 <table className='table mytable table-striped border table-hover mt-4'>
@@ -30,15 +117,46 @@ const Generos = () => {
                         </tr>
                     </thead>
                     <tbody className='tbody'>
+                        <tr className='tr'>
+                            <td className='td'>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nuevo nombre"
+                                    name="nuevonombre"
+                                    value={generoNombre}
+                                    onChange={(e) => setGeneroNombre(e.target.value)}
+                                    required
+                                />
+                            </td>
+                            <td className='td'>
+                                <button className='d-flex align-items-center btn btn-primary p-2 mx-2' onClick={(e) => agregarGenero(e, generoNombre)}>
+                                    <img src={iconoGuardar} alt="" style={{ height: '20px' }} />
+                                </button>
+                            </td>
+
+                        </tr>
                         {generos.map((el, id) => (
 
                             <tr className='tr' key={id}>
                                 <td className='td' data-label='Nombre'>
-                                    {el.nombre}
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Nuevo nombre"
+                                        name="nuevonombre"
+                                        value={el.nombre}
+                                        onChange={
+                                            (e) => {const nuevaLista = [...generos];
+                                                nuevaLista[id] = { ...el, nombre: e.target.value };
+                                                setGeneros(nuevaLista);}
+                                        }
+                                        required
+                                    />
                                 </td>
                                 <td className='td' data-label='Imagen'>
-                                    <button className='d-flex align-items-center btn btn-primary p-2 mx-2' onClick={() => editarOnClick(el)}>
-                                        <img src={iconoEditar} alt="" style={{ height: '20px' }} />
+                                    <button className='d-flex align-items-center btn btn-primary p-2 mx-2' onClick={(e) => editarGenero(e, el)}>
+                                        <img src={iconoGuardar} alt="" style={{ height: '20px' }} />
                                     </button>
                                 </td>
                             </tr>
@@ -48,7 +166,9 @@ const Generos = () => {
 
                 </table>
             }
+            <Toast tipo={toast.tipo} titulo={toast.titulo} mensaje={toast.mensaje} visible={toast.visible}></Toast>
         </div>
+
     )
 
 }
