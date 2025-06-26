353261ec08d2563bde7c3a68cf28fe22a476cf26
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-06-26 18:45:55 -0500

    feat: :sparkles: buscar película

--- src/B usuarioInterno/Peliculas/VentanaPeliculas.jsx
+++ src/B usuarioInterno/Peliculas/VentanaPeliculas.jsx
@@ -62,6 +62,7 @@ const VentanaPeliculas = () => {
 
     useEffect(() => {
         console.log(lista)
+        setResultados(lista)
     }, [lista])
 
     const editarOnClick = (pelicula) => {
@@ -73,61 +74,107 @@ const VentanaPeliculas = () => {
         consultarPeliculas();
     }
 
+    const [busqueda, setBusqueda] = useState('');
+    const [resultados, setResultados] = useState();
+
+
+    const manejarCambio = (e) => {
+        const texto = e.target.value;
+        setBusqueda(texto);
+        if (texto.length > 0) {
+            const filtrados = lista.filter(item =>
+                item.nombre.toLowerCase().includes(texto.toLowerCase())
+            );
+            setResultados(filtrados);
+        } else {
+            setResultados(lista);
+        }
+        console.log("resultados", resultados)
+
+    };
+
+    const seleccionar = (item) => {
+        const datos = item.split('|').map(e => e.trim())
+        console.log(datos)
+        const encontrado = productos.find(el => el.nombre == datos[0])
+        setResultados([]);
+        setLista(prev => [...prev, encontrado]);
+        setBusqueda()
+    };
+
     return (
         <div>
-            <div className='d-block'>
+            <div className='d-flex flex-column gap-4 align-items-center'>
                 <AddFilm onSucess={consultarPeliculas}></AddFilm>
 
                 {loading === true
                     ? <div className='d-flex flex-column align-items-center container'><Loading></Loading></div> :
-                    <table className='table mytable table-striped border table-hover mt-4'>
-                        <thead className='thead'>
-                            <tr>
-                                <th className='td'>Nombre</th>
-                                <th className='td'>Duración (min)</th>
-                                <th className='td'>Sinopsis</th>
-                                <th className='td'>Clasificación</th>
-                                <th className='td'>Estado</th>
-                                <th className='td'>Inicio de estreno</th>
-                                <th className='td'>Imagen</th>
-                                <th className='td'>Imagen</th>
-                            </tr>
-                        </thead>
-                        <tbody className='tbody'>
-                            {lista.map((el, id) => (
 
-                                <tr className='tr' key={id}>
-                                    <td className='td' data-label='Nombre'>
-                                        {el.nombre}
-                                    </td>
-                                    <td className='td' data-label='Duración'>
-                                        {el.duracion}
-                                    </td>
-                                    <td className='td' data-label='Sinopsis'><div className='sinopsis' style={{ width: '200px' }}>{el.sinopsis}</div></td>
-                                    <td className='td' data-label='Clasificación'>{el.clasificacion === "" | el.clasificacion === " " ? "-" : el.clasificacion}</td>
-                                    <td className='td' data-label='Estado'>{el.estado}</td>
-                                    <td className='td' data-label='Inicio de estreno'>
-                                        {el.fechaInicioEstreno ? format(parse(el.fechaInicioEstreno, 'yyyy-MM-dd', new Date()), 'dd/MM/yyyy') : ''}
-                                    </td>
-                                    <td className='td' data-label='Imagen'>
-                                        <a href={el.imageUrl} target="_blank" rel="noopener noreferrer">
-                                            <strong>Enlace</strong>
-                                        </a>
+                    <div>
+                        <div className='hstack justify-content-center gap-2 m-4'>
+                            <h3>Buscar película</h3>
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder='Nombre de película'
+                                    className='form-control'
+                                    value={busqueda}
+                                    onChange={manejarCambio}
+                                    style={{ width: '300px' }}
+                                />
+                            </div>
+                        </div>
 
-                                    </td>
-                                    <td>
-                                        <button className='d-flex align-items-center btn btn-primary p-2 mx-2' onClick={() => editarOnClick(el)}>
-                                            <img src={iconoEditar} alt="" style={{ height: '20px' }} />
-                                        </button>
-                                    </td>
+                        <table className='table mytable table-striped border table-hover mt-4'>
+                            <thead className='thead'>
+                                <tr>
+                                    <th className='td'>Nombre</th>
+                                    <th className='td'>Duración (min)</th>
+                                    <th className='td'>Sinopsis</th>
+                                    <th className='td'>Clasificación</th>
+                                    <th className='td'>Estado</th>
+                                    <th className='td'>Inicio de estreno</th>
+                                    <th className='td'>Imagen</th>
+                                    <th className='td'>Imagen</th>
                                 </tr>
+                            </thead>
+                            <tbody className='tbody'>
+                                {resultados.map((el, id) => (
 
-                            ))}
-                        </tbody>
+                                    <tr className='tr' key={id}>
+                                        <td className='td' data-label='Nombre'>
+                                            {el.nombre}
+                                        </td>
+                                        <td className='td' data-label='Duración'>
+                                            {el.duracion}
+                                        </td>
+                                        <td className='td' data-label='Sinopsis'><div className='sinopsis' style={{ width: '200px' }}>{el.sinopsis}</div></td>
+                                        <td className='td' data-label='Clasificación'>{el.clasificacion === "" | el.clasificacion === " " ? "-" : el.clasificacion}</td>
+                                        <td className='td' data-label='Estado'>{el.estado}</td>
+                                        <td className='td' data-label='Inicio de estreno'>
+                                            {el.fechaInicioEstreno ? format(parse(el.fechaInicioEstreno, 'yyyy-MM-dd', new Date()), 'dd/MM/yyyy') : ''}
+                                        </td>
+                                        <td className='td' data-label='Imagen'>
+                                            <a href={el.imageUrl} target="_blank" rel="noopener noreferrer">
+                                                <strong>Enlace</strong>
+                                            </a>
 
-                    </table>
+                                        </td>
+                                        <td>
+                                            <button className='d-flex align-items-center btn btn-primary p-2 mx-2' onClick={() => editarOnClick(el)}>
+                                                <img src={iconoEditar} alt="" style={{ height: '20px' }} />
+                                            </button>
+                                        </td>
+                                    </tr>
+
+                                ))}
+                            </tbody>
+
+                        </table>
+                    </div>
                 }
             </div>
+
 
             {pelicula && <PeliculaModal pelicula={pelicula} onCerrar={onPeliModalCerrar} />}
         </div>
