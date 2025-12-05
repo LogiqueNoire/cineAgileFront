2ed52e3ecb423b2901d97eeaedd992bcfc0c7a08
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2025-12-05 08:06:57 -0500

    fix: bg-black

--- src/Intranet/LoginForm.jsx
+++ src/Intranet/LoginForm.jsx
@@ -57,7 +57,7 @@ const LoginForm = () => {
 
     return (
         <div className="d-flex justify-content-center align-items-center" style={{ marginTop: '25vh', marginBottom: '25vh' }}>
-            <div className="bg-dark" style={{ width: '100vw', height: '100vh', position: 'fixed', zIndex: '-50' }}></div>
+            <div className="bg-black" style={{ width: '100vw', height: '100vh', position: 'fixed', zIndex: '-50' }}></div>
             <img src={imagenFondo} alt="" style={{ position: 'fixed', zIndex: '-30' }}/>
             <video src={videoFondo} loop autoPlay muted playsInline style={{ position: 'fixed', zIndex: '-20' }}></video>
             <form className='m-4 mt-0 border border-1 p-5 rounded rounded-4'
