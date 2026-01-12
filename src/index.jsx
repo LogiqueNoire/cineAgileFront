d19f5e8ba0875a3bc045ac60ce97bba8e462df43
LeandroMauricci <139007271+LeandroMauricci@users.noreply.github.com>
2026-01-12 15:04:54 -0500

    fix: :adhesive_bandage: showToast en intranet

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from '@/App.jsx';
+import App from '@/Venta/App.jsx';
 
 // Componentes de venta
 import PeliculaSedes from '@/Venta/3 componentesVenta/PeliculaSedes.jsx';
@@ -29,42 +29,45 @@ import Generos from '@/Intranet/Generos/Generos.jsx';
 import Auditoria from '@/Intranet/Auditoria/Auditoria.jsx';
 import OpcionesDesarrollador from '@/Intranet/Desarrollador/OpcionesDesarrollador.jsx'
 import FilmPanel from '@/Venta/1 componentesCartelera/FilmPanel';
+import { ToastContextProvider } from "./context/ToastContextProvider";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-            { /* Ruta principal: La idea es que el componente App muestra el contenido de las rutas
+    <ToastContextProvider>
+        <BrowserRouter>
+            <Routes>
+                { /* Ruta principal: La idea es que el componente App muestra el contenido de las rutas
                 hijas (ej: Inicio) dependiendo de la ruta. En este caso, Inicio es lo que se motrará en la
                 ruta por defecto. Esto nos permite cambiar entre páginas sin necesidad de recargar. */ }
-            <Route path='/' element={<App />}>
-                <Route index element={<FilmPanel />} />
-                <Route path='/funcion/pelicula/' element={<PeliculaSedes />} />
-                <Route path='/compra' element={<VentaContextProvider><FlujoVenta /></VentaContextProvider>} />
-                <Route path='/entradas' element={<InfoEntradas />} />
-                <Route path='/error' element={<Error />} />
-                <Route path="/entrada/:codigo" element={<InfoEntradas />}></Route>
-            </Route>
+                <Route path='/' element={<App />}>
+                    <Route index element={<FilmPanel />} />
+                    <Route path='/funcion/pelicula/' element={<PeliculaSedes />} />
+                    <Route path='/compra' element={<VentaContextProvider><FlujoVenta /></VentaContextProvider>} />
+                    <Route path='/entradas' element={<InfoEntradas />} />
+                    <Route path='/error' element={<Error />} />
+                    <Route path="/entrada/:codigo" element={<InfoEntradas />}></Route>
+                </Route>
 
 
-            <Route path='/intranet' element={<Intranet />}>
-                <Route index element={<IntranetPanel />}></Route>
-                <Route path='login' element={<LoginForm />}></Route>
-                <Route path="peliculas" element={<VentanaPeliculas />}></Route>
-                <Route path="sedesysalas" element={<VentanaSedesYSalas />}></Route>
-                <Route path="sala" element={<Sala />}></Route>
-                <Route path="funciones" element={<FuncionesContextProvider><VentanaFunciones /></FuncionesContextProvider>}></Route>
-                <Route path='generos' element={<Generos />}></Route>
-                <Route path="usuarios" element={ <VentanaUsuario /> }></Route>
-                <Route path='ajustes' element={<VentanaAjustes />}></Route>
-                <Route path='analiticas' element={<Analiticas />}></Route>
-                <Route path='auditoria' element={<Auditoria />}></Route>
-                <Route path="salas" /*element={<VentanaSedesYSalas></VentanaSedesYSalas>}*/></Route>
-                <Route path='dev' element={<OpcionesDesarrollador />}></Route>
-            </Route>
+                <Route path='/intranet' element={<Intranet />}>
+                    <Route index element={<IntranetPanel />}></Route>
+                    <Route path='login' element={<LoginForm />}></Route>
+                    <Route path="peliculas" element={<VentanaPeliculas />}></Route>
+                    <Route path="sedesysalas" element={<VentanaSedesYSalas />}></Route>
+                    <Route path="sala" element={<Sala />}></Route>
+                    <Route path="funciones" element={<FuncionesContextProvider><VentanaFunciones /></FuncionesContextProvider>}></Route>
+                    <Route path='generos' element={<Generos />}></Route>
+                    <Route path="usuarios" element={<VentanaUsuario />}></Route>
+                    <Route path='ajustes' element={<VentanaAjustes />}></Route>
+                    <Route path='analiticas' element={<Analiticas />}></Route>
+                    <Route path='auditoria' element={<Auditoria />}></Route>
+                    <Route path="salas" /*element={<VentanaSedesYSalas></VentanaSedesYSalas>}*/></Route>
+                    <Route path='dev' element={<OpcionesDesarrollador />}></Route>
+                </Route>
 
-            <Route path='/butacas' element={<ButacaSelect idSala={5} />} />
+                <Route path='/butacas' element={<ButacaSelect idSala={5} />} />
 
 
-        </Routes>
-    </BrowserRouter>
+            </Routes>
+        </BrowserRouter>
+    </ToastContextProvider>
 );
